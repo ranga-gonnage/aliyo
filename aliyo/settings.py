4fcf1fc1e22605a7a01d7d5860d0bcc6997307c6
Ranga Gonnage <rangagonnage@MacBook-Pro-de-Ranga.local>
2023-02-16 13:29:52 +0100

    Add hosts aliyo.fr

--- aliyo/settings.py
+++ aliyo/settings.py
@@ -32,7 +32,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["aliyo.herokuapp.com", "127.0.0.1", "0.0.0.0",]
+ALLOWED_HOSTS = ["aliyo.herokuapp.com", "127.0.0.1", "0.0.0.0", "www.aliyo.fr"]
 
 
 # Application definition
