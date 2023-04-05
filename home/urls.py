f6bd39e234eeac1e592884d22f3541927b972f36
Ranga Gonnage <rangagonnage@MacBook-Pro-de-Ranga.local>
2023-04-05 18:02:30 +0200

    Add legal notice and privacy policy

--- home/urls.py
+++ home/urls.py
@@ -5,4 +5,6 @@ from home import views
 app_name = "home"
 urlpatterns = [
     path("", views.Home.as_view(), name="home"),
+    path("mentions-legales/", views.LegalNotice.as_view(), name="legalNotice"),
+    path("politique-de-confidentialite/", views.PrivacyPolicy.as_view(), name="privacyPolicy"),
 ]
--- home/views.py
+++ home/views.py
@@ -6,5 +6,15 @@ class Home(View):
         context = {}
         return render(request, "home.html", context)
 
+class LegalNotice(View):
+    def get(self, request):
+        context = {}
+        return render(request, "legalNotice.html", context)
+
+class PrivacyPolicy(View):
+    def get(self, request):
+        context = {}
+        return render(request, "privacyPolicy.html", context)
+
 def error_404_view(request, exception):
     return render(request, '404.html')
